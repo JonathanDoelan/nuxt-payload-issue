84b22611edeb9f8e5f4d2a70dcd82eb82b78178b
JonathanDoelan <2291224+JonathanDoelan@users.noreply.github.com>
2023-01-20 12:27:09 +0000

    minimum

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>{{ data }}</p>
+    </div>
+</template>
+
+
+
+<script setup>
+const { data } = await useAsyncData('about', () => ({
+    subpage: {
+        title: 'about me'
+    }
+}));
+
+useHead({ title: data.value.subpage.title });
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>{{ data }}</p>
+    </div>
+</template>
+
+
+
+<script setup>
+const { data } = await useAsyncData('index', () => ({
+    index: {
+        page: 'home'
+    }
+}));
+
+useHead({ title: data.value.index.page });
+</script>
